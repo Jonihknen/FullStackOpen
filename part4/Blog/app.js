9ff8d5c2e928192b68e940a914fed0a550bf2c23
Joni <jonihknen@gmail.com>
2022-06-09 00:51:03 +0300

    part4 done

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 var _ = require('lodash')
+
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -20,8 +24,11 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/Blog/controllers/blogs.js
+++ part4/Blog/controllers/blogs.js
@@ -1,39 +1,79 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
 
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
+  response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
+blogsRouter.get('/:id', async (request, response) => {
+  const note = await Blog.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+
+  const user = request.user
+
+  const blogToDelete = await Blog.findById(request.params.id)
+
+  if (blogToDelete.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'No permission to remove blog' })
+  }
+  if (blogToDelete.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
+})
+
+blogsRouter.post('/',  middleware.userExtractor, async (request, response) => {
   const body = request.body
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
+
+  const user = request.user
+
+  if (!body.title && !body.url) {
+    return response.status(400).json({
+      error: 'tittle and url missing'
     })
+  } else {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ? body.likes : 0,
+      user: user._id
+    })
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(blog)
+  }
 })
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body,
+    { new: true })
+  response.json(updatedBlog)
+})
+
 
 module.exports = blogsRouter
