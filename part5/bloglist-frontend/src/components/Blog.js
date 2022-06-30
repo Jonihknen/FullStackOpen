82d2fa3e54fcb2f41e49ada4fb83fc4364aa9310
Joni <jonihknen@gmail.com>
2022-06-30 11:27:32 +0300

    part b done

---
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import blogService from '../services/blogs'
+
+const Blog = ({ blog, user }) => {
+
+  const [visible, setVisible] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
+  const [isSameUser, setSameUser] = useState(false)
+
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const showDeleteButton = { display: isSameUser ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const handleClick = () => {
+    setVisible(!visible)
+
+    if (blog.user.username === user.username) {
+      setSameUser(true)
+    }
+  }
+
+  const handleLikes = async (event) => {
+    event.preventDefault()
+    const newBlog = {
+      ...blog,
+      likes: likes + 1
+    }
+    const response = await blogService.update(newBlog)
+    setLikes(response.likes)
+  }
+
+  const handleDelete = async (event) => {
+    event.preventDefault()
+    if (window.confirm(`delete blog ${blog.title} ?`)) {
+      const response = await blogService.remove(blog.id)
+      console.log(response)
+    }}
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} <button onClick={handleClick}> view </button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} <button onClick={handleClick}> hide </button>
+        <div>{blog.url}</div>
+        <div>likes {likes} <button onClick={handleLikes}> like </button></div>
+        <div>{blog.author}</div>
+        <div style={showDeleteButton }><button onClick={handleDelete}> remove </button></div>
+      </div>
+    </div>
+  )}
+
+export default Blog
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LoginForm = ({ onSubmit, username, password, setUsername, setPassword }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+          username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+          password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
+
+export default LoginForm
